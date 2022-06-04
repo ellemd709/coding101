23cf1f6bf0bf1d3868c0a7f85eac1c6c327dc23d
Lisa Doody <101128408+ellemd709@users.noreply.github.com>
2022-06-04 01:02:48 +0000

    testing js

--- script.js
+++ script.js
@@ -1,8 +1,36 @@
 function sayhello() {
   alert('Hello, Welcome')
 }
+var myname='Lisa';
+console.log("My name is:")
+console.log(myname)
 
 
+var one = 1
+var two = 2
+var three = 3
+
+console.log(1+2+3)
+
+//string
+
+var one = "one";
+var two = "two";
+var three = "three";
+
+console.log(one+two+three)
+
+let day = "Monday";
+let flavor = "Chocolate";
+
+if (day=== "monday") {
+  let flavor = "Vanilla";
+
+  console.log('it is Monday. Have a scoop of ${flavor} ice cream.');
+}
+
+console.log('it is not Monday. Have a scoop of ${flavor} ice cream.');
+//why isn't the flavor showing 
 
 
 
