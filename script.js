1ead238a10b6b768d65de9b94a0c2d79ad03706e
Lisa Doody <101128408+ellemd709@users.noreply.github.com>
2022-07-25 13:33:45 +0000

    slice and splice

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function sayhello() {
   alert('Hello, Welcome')
 }
-let myname='Lisa';
+let myname = 'Lisa';
 console.log("My name is:")
 console.log(myname)
 
@@ -10,20 +10,19 @@ let one = 1
 let two = 2
 let three = 3
 
-console.log(1+2+3)
+console.log(1 + 2 + 3)
 
 //string
 
-let one = "one";
-let two = "two";
-let three = "three";
-
-console.log(one+two+three)
+//let one = "one";
+//let two = "two";
+//let three = "three";
+//console.log(one + two + three)
 
 let day = "Monday";
 let flavor = "Chocolate";
 
-if (day=== "monday") {
+if (day === "monday") {
   let flavor = "Vanilla";
 
   console.log('it is Monday. Have a scoop of ${flavor} ice cream.');
@@ -32,8 +31,13 @@ if (day=== "monday") {
 console.log('it is not Monday. Have a scoop of ${flavor} ice cream.');
 //why isn't the flavor showing 
 
+// slice and splice 
+
+var users = ["Tim", "Tom", "Bob", "Frank"];
+//console.log(users.slice(1, 3));
+users.splice(1, 2, "hello");
+console.log(users);
 
 
 
 
-      
